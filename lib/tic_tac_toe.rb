710dd1ead76fe3d42639b4ac75adb9a9a08c133b
Agnieszka Letkiewicz <a.letkiewicz@gmail.com>
2020-04-14 20:29:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,3 +64,32 @@ def current_player(board)
     return "O"
   end
 end 
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combination|
+    position_taken?(board, combination[0]) && (board[combination[0]] == board[combination[1]]) && (board[combination[1]] == board[combination[2]])
+  end
+end
+
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O"
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  full?(board) || draw?(board) || won?(board)
+end
+
+def winner(board)
+  if won?(board)
+    winning_array = won?(board)
+    board[winning_array[0]]
+  else 
+    return nil 
+  end
+end
