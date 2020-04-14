ac68bdfa56f41faf3e180cd0d34f06d07d078d76
Agnieszka Letkiewicz <a.letkiewicz@gmail.com>
2020-04-14 20:29:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,3 +45,22 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0 
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1 
+    else
+      counter += 0 
+    end
+  end
+  return counter 
+end
+
+def current_player(board)
+  if turn_count(board).even? 
+    return "X"
+  else
+    return "O"
+  end
+end 
